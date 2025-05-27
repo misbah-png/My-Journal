fb8e9fb3bad94b87519e46cc4a7c86eb36bfdf64
misbah-png <misbaaaslam@gmail.com>
2025-05-27 18:18:17 +0400

    add redirect

--- front/vite.config.js
+++ front/vite.config.js
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', 
   build: {
     outDir: 'dist',
   },
   server: {
-    historyApiFallback: true,
-  }
+     
+  },
 })
+
